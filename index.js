3de4a031f3744264378708104898dc0b00e3101c
Loren <lorensprague@icloud.com>
2022-07-19 02:07:33 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(callback) {
+    return callback();
+}
+
+
+function returnsANamedFunction() {
+    return function aNamedFunction() {
+    };
+}
+
+function returnsAnAnonymousFunction() {
+    return function () {
+    }
+}
